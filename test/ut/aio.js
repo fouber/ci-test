a78a4e58f9e3ce45c4b0bb0c4d588a9dc11c1b44
zhangyunlong <zhangyunlong@baidu.com>
2013-09-05 13:30:23 +0800

    add case

--- test/ut/aio.js
+++ test/ut/aio.js
@@ -1,6 +1,11 @@
 var ci = require('../../index.js');
 var assert = require('assert');
+
+console.log('start');
+
 assert.equal(ci.a, 123, 'ci-test.a');
+console.log('case 1');
+
 assert.equal(
     ci._.filter(
         [1, 2, 3, 4, 5, 6],
@@ -11,3 +16,6 @@ assert.equal(
     [2, 4, 6],
     'ci-test._.filter'
 );
+console.log('case 2');
+
+console.log('end');
