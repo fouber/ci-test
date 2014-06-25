4ec4429295b59297fe4af495f33f8ec6782f298b
fouber <fouber.NET@gmail.com>
2014-06-25 20:26:22 +0800

    update yml

--- build.sh
+++ build.sh
@@ -4,17 +4,13 @@ if [ -d output ]; then
     echo '➥ Packing...'
     cd output
     tar zcvf ${output_file} **
-    mv ${output_file} ..
-    cd ..
-    rm -rf output
     
     echo "➥ Commit file [${output_file}]"
-    git remote set-url origin https://github.com/${REPO_SLUG}.git
+    git clone --depth=50 --branch=deploy https://github.com/${REPO_SLUG}.git ${REPO_SLUG}
+    mv ${output_file} ${REPO_SLUG}
+    cd ${REPO_SLUG}
     git config user.email "travis@travis-ci.org"
     git config user.name "travis-ci"
-    git fetch origin deploy
-    git checkout -b deploy
-    git pull origin deploy
     git add ${output_file}
     git commit -m "[ci skip] release ${output_file}"
     git push origin deploy
