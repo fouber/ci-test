a7eb12ce85f8b172749d11666fbbdf68faf1f77c
fouber <fouber.NET@gmail.com>
2014-06-25 13:51:14 +0800

    release 2014.06.25-13.51.14.tar.gz

--- fis-conf.js
+++ fis-conf.js
@@ -1,4 +1,4 @@
-fis.config.set('project.exclude', 'node_modules');
+fis.config.set('project.exclude', /\/node_modules\//i);
 fis.config.set('roadmap.path', [
     {
         reg: /\/(LICENSE|README\.md|build\.(sh|bat|cmd))$/i,
