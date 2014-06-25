1a43bf179baafcfcaf825d04fc8ca4f91432e09d
fouber <fouber.NET@gmail.com>
2014-06-25 13:55:11 +0800

    update

--- fis-conf.js
+++ fis-conf.js
@@ -3,5 +3,9 @@ fis.config.set('roadmap.path', [
     {
         reg: /\/(LICENSE|README\.md|build\.(sh|bat|cmd))$/i,
         release: false
+    },
+    {
+        reg: 'map.json',
+        release: false
     }
 ]);
