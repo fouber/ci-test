57a250813ded443187def3e6ade3c2dfcf50431e
fouber <fouber.NET@gmail.com>
2014-06-25 20:32:43 +0800

    update yml

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ if [ -d output ]; then
     tar zcvf ${output_file} **
     
     echo "➥ Commit file [${output_file}]"
-    git clone --depth=50 --branch=deploy https://${GH_TOKEN}@github.com/${REPO_SLUG}.git ${REPO_SLUG}
+    git clone --quiet --branch=deploy https://${GH_TOKEN}@github.com/${REPO_SLUG}.git ${REPO_SLUG} > /dev/null
     mv ${output_file} ${REPO_SLUG}
     cd ${REPO_SLUG}
     git config user.email "travis@travis-ci.org"
     git config user.name "travis-ci"
-    git add ${output_file}
+    git add -f ${output_file}
     git commit -m "[ci skip] release ${output_file}"
-    git push origin deploy
+    git push -fq origin deploy > /dev/null
 else
     echo '➥ Error.'
     exit 1
